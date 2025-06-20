69874e376ef9e1d73563b2d573353f06c183210f
ben.mccarthy <ben.mccarthy@iliad-solutions.com>
2025-06-20 13:49:11 +0100

    Creates login form component - it provides validation and will be responsive once the form has been completed

--- angular-to-do-list/src/app/components/login-form/login-form.component.ts
+++ angular-to-do-list/src/app/components/login-form/login-form.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 export interface Account {
   id: number;
@@ -10,7 +11,7 @@ export interface Account {
 
 @Component({
   selector: 'app-login-form',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.css',
 })
@@ -19,13 +20,30 @@ export class LoginFormComponent {
   newEmail: string = '';
   newPassword: string = '';
   newUsername: string = '';
+  accountCreated: boolean = false;
+  passwordValidator: RegExp = /^(?=.*[A-Z])(?=.*\d).{8,}$/;
+  submitPressed: boolean = false;
+  validPassword: boolean = false;
 
   createAccount(): void {
-    if (this.newEmail.trim() !== '' && this.newUsername && this.newPassword) {
-      this.newAccount.id = Date.now();
-      this.newAccount.email = this.newEmail.trim();
-      this.newAccount.password = this.newPassword;
-      this.newAccount.username = this.newUsername.trim();
+    if (this.newEmail.trim() && this.newPassword && this.newUsername.trim()) {
+      this.validPassword = this.passwordValidator.test(this.newPassword);
+      this.submitPressed = true;
+      if (this.validPassword) {
+        this.newAccount.id = Date.now();
+        this.newAccount.email = this.newEmail.trim();
+        this.newAccount.password = this.newPassword;
+        this.newAccount.username = this.newUsername.trim();
+
+        this.accountCreated = true;
+        this.newEmail = '';
+        this.newPassword = '';
+        this.newUsername = '';
+      }
     }
+  }
+
+  showUsername() {
+    return this.newAccount.username;
   }
 }
